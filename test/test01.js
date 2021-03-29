009eda1f401361e786f6c23fe07a38f27f3ffc9b
Miguel Angel -Cheroki- <miguelangelmorato84@gmail.com>
2021-03-29 10:32:40 +0200

    test01.js completed

--- test/test01.js
+++ test/test01.js
@@ -1,4 +1,31 @@
-function geekshubs(index) {
-    return "";
+function geekshubs(iterations) {
+
+    let arrayInicial = [];
+
+    for (let i = 1; i <= iterations; i++) {
+        arrayInicial.push(i);
+    }
+
+    let arrayModificado = arrayInicial.map((valor) => {
+        let resto3 = valor % 3
+        let resto5 = valor % 5
+
+        if (resto3 == 0) {
+            valor = 'Geeks';
+        } else if (resto5 == 0) {
+            valor = 'Hubs';
+        }
+
+        if (resto3 == 0 & resto5 == 0) {
+            valor = 'GeeksHubs';
+        }
+
+        return valor;
+    })
+
+    let arrayUnido = arrayModificado.map((linea) => linea + "\n")
+
+    return arrayUnido.join("");
 }
+
 module.exports = geekshubs;
